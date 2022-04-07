1929b313a679780ab40bf51b613ee7253caf22d5
matheusmcz <matheusmczvieira@gmail.com>
2022-04-07 00:28:01 -0300

    chore: Architecture adjustment

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "./styles/global.css";
+import Views from "./views";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Views />
   </React.StrictMode>,
   document.getElementById("root")
 );
